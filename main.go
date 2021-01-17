a58850ae4fdd7340061961d3d6e6b903186d204f
longjymaker1 <915268001@qq.com>
2021-01-17 22:49:49 +0800

    create tables

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	_ "GoJcShare/models"
 	_ "GoJcShare/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
 )
 
+func init() {
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	maxIdle := 500
+	maxConn := 500
+	orm.RegisterDataBase("default", "mysql",
+		"root:123qwe@tcp(127.0.0.1:3306)/go_jc_share?charset=utf8&loc=Asia%2FShanghai",
+		maxIdle, maxConn)
+}
+
 func main() {
+	orm.Debug = true
+	orm.RunSyncdb("default", false, true)
+
 	beego.Run()
 }
--- models/models.go
+++ models/models.go
@@ -1,1 +1,29 @@
 package models
+
+import (
+	"github.com/astaxie/beego/orm"
+	"time"
+)
+
+type Users struct {
+	Id            int64           `pk:"auto;column(id)"`
+	Name          string          `orm:"size(32);column(name);comment('昵称')"`
+	Email         string          `orm:"size(64);column(email);comment('邮箱')"`
+	Status        int64           `orm:"size(10);column(status);default(1);comment('状态,正常1,异常0')"`
+	Integral      int64           `orm:"size(10);column(integral);comment('积分,RMB:积分,1:1');default(0)"`
+	CreateTime    time.Time       `orm:"auto_now_add;type(datetime);column(create_time);comment('创建时间')"`
+	UpdateTime    time.Time       `orm:"auto_now;type(datetime);column(update_time);comment('更新时间')"`
+	UserLoginInfo *UsersLoginInfo `orm:"rel(one)"`
+}
+
+type UsersLoginInfo struct {
+	Id        int64  `pk:"auto;column(id)"`
+	UserID    int64  `orm:"column(user_id);comment('用户id')"`
+	LoginName string `orm:"size(16);column(login_name);comment('用户登录账户')"`
+	Pwd       string `orm:"size(32);column(password);comment('用户登录密码')"`
+	User      *Users `orm:"reverse(one)"`
+}
+
+func init() {
+	orm.RegisterModel(new(Users), new(UsersLoginInfo))
+}
